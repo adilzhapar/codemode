4719564c2bec2f5f51dd1e93260ffe46f79a48a7
Adil Zhapar <adilzhapar2002@gmail.com>
2021-11-25 22:09:37 +0600

    upd

--- 25.11/fence.cpp
+++ 25.11/fence.cpp
@@ -1,10 +1,31 @@
+// Fence
 #include <iostream>
-
+#include <set>
+#include <vector>
+#include <stack>
 using namespace std;
 
-int main(){
-    
-    
+void Fence(int n){
+    stack < int > st;
+    set < int > s;
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        while(st.size() > 0 && st.top() < a[i]){
+            st.pop();
+        }
+        st.push(a[i]);
+    }
+    while(st.size() > 0){
+        s.insert(st.top());
+        st.pop();
+    }
+    cout << s.size();
+}
 
+int main(){
+    int n;
+    cin >> n;
+    Fence(n);
     return 0;
 }
